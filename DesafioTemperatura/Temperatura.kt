1c45e9b90a8ab8238a82f06e472bd7d6c2ddbfa5
AugustoCVS <guty.vertolis@gmail.com>
2023-08-23 20:37:11 -0300

    feat: create a feat to calculate the temperature

---
+++ DesafioTemperatura/Temperatura.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    var celsius: Double = 25.0
+    var fahrenheit: Double = celsius * 9/5 + 32
+    var kelvin: Double = celsius + 273.15
+
+println("A temperatura em celsius é: $celsius")
+println("A temperatura em fahrenheit é: $fahrenheit")
+println("A temperatura em kelvin é: $kelvin")
+
+}
